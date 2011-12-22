9cffc4c0f3de77f5400b11ab92fd15462171920f
simonpai <simonpai@cea5895d-b19a-9f65-2904-9ce78ca98dfd>
2011-12-22 06:22:43 +0000

    Canvas use proxy list to handle Drawables

---
+++ canvas4z/src/test/java/org/zkoss/canvas/test/ListTestController.java
@@ -0,0 +1,41 @@
+/* ListTestController.java
+
+{{IS_NOTE
+ Purpose:
+  
+ Description:
+  
+ History:
+  Dec 22, 2011 10:39:48 AM , Created by simonpai
+}}IS_NOTE
+
+Copyright (C) 2011 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.canvas.test;
+
+import org.zkoss.canvas.Canvas;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+/**
+ *
+ * @author simonpai
+ */
+public class ListTestController extends GenericForwardComposer {
+	
+	private static final long serialVersionUID = 3717700698195665603L;
+	
+	private Canvas cvs;
+	
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		
+		
+		
+	}
+	
+}
